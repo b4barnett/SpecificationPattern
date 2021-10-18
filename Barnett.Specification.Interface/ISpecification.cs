a64b525b89335cf7fe8b059e42316e4dadf4e92a
Daniel Barnett <b4barnett@googlemail.com>
2021-10-18 21:31:16 +0100

    Create project structure

---
+++ Barnett.Specification.Interface/ISpecification.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Barnett.Specification.Interface
+{
+    public interface ISpecification<T>
+    {
+        bool Matches( T value );
+    }
+
+    public interface ISpecificationAsync<T>
+    {
+        Task<bool> Matches( T value );
+    }
+}
